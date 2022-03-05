173e698231c97e9474f2238aa70655c57cc43980
NickNocc <haretakis4@gmail.com>
2022-03-05 12:04:39 -0800

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -48,4 +48,19 @@ function ContactForm() {
           <label htmlFor="email">Email address:</label>
           <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
- 
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
