fd28fd27f24830e9a7cfa3c2353df3391078f37f
NickNocc <haretakis4@gmail.com>
2022-03-05 12:04:22 -0800

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,3 +5,14 @@ import { validateEmail } from '../../utils/helpers';
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+ 
