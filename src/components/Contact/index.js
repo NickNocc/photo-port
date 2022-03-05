10484f1fea71eb0525ff82cce728ad5ede9225a9
NickNocc <haretakis4@gmail.com>
2022-03-05 12:04:28 -0800

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -15,4 +15,25 @@ function ContactForm() {
     }
   };
 
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
  
